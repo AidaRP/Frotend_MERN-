8da50c448731a8ab75943193176c9f7d380be731
AidaRP <90700748+AidaRP@users.noreply.github.com>
2022-03-26 16:47:39 +0100

    form register created

---
+++ src/components/Register/Register.jsx
@@ -0,0 +1,75 @@
+import { Form, Input, Button } from "antd";
+import "antd/dist/antd.css";
+
+const Register = () => {
+  const onFinish = (values) => {
+    console.log("Success:", values);
+  };
+
+  return (
+    <Form
+      name="basic"
+      labelCol={{
+        span: 8,
+      }}
+      wrapperCol={{
+        span: 16,
+      }}
+      initialValues={{
+        remember: true,
+      }}
+      onFinish={onFinish}
+      autoComplete="off"
+    >
+      <Form.Item
+        label="Nickname"
+        name="nickname"
+        rules={[
+          {
+            required: true,
+            message: "Please input your Nickname!",
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+      <Form.Item
+        label="Email"
+        name="email"
+        rules={[
+          {
+            required: true,
+            message: "Please input your Email!",
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+      <Form.Item
+        label="Password"
+        name="password"
+        rules={[
+          {
+            required: true,
+            message: "Please input your password!",
+          },
+        ]}
+      >
+        <Input.Password />
+      </Form.Item>
+
+      <Form.Item
+        wrapperCol={{
+          offset: 8,
+          span: 16,
+        }}
+      >
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+      </Form.Item>
+    </Form>
+  );
+};
+
+export default Register;
