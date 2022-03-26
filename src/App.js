74b55c3b6900072c5fc300b8b78471aec56bd7e9
AidaRP <90700748+AidaRP@users.noreply.github.com>
2022-03-26 19:41:04 +0100

    login route created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Register from './components/Register/Register'
+import Login from './components/Login/Login';
 function App() {
   return (
     <div className="App">
    <BrowserRouter>
         <Routes>
           <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+
           </Routes>
           </BrowserRouter>
     </div>
