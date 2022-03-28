f99b0a6c74bf986f441efa67a31ccfea5af60e0c
raquelMoya <rakelms90@gmail.com>
2022-03-28 16:00:00 +0200

    actions in user.js added

--- src/redux/actions/user.js
+++ src/redux/actions/user.js
@@ -1,29 +1,98 @@
 import store from "../store";
 import axios from "axios";
 import { API_URL } from "../../utility";
+import {
+  LOGIN,
+  MODIFY_CREDENTIALS,
+  USER_INFO,
+  GET_USERS,
+  DELETE_USER,
+} from "../types";
+
+
 export const register = async (dataUser) => {
-    try {
-        let res = await axios.post(API_URL + "/users/register", dataUser);
-        return res;
-    } catch (error) {
-        console.log(error);
-    }
+  try {
+    let res = await axios.post(API_URL + "/users/register", dataUser);
+    return res;
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export const login = async (user) => {
-    try {
-        let res = await axios.post(API_URL + "/users/login", user);
-        return res;
-    } catch (error) {
-        console.log(error);
-    }
+  try {
+    const res = await axios.post(API_URL + "/users/login", user);
+    store.dispatch({
+      type: LOGIN,
+      payload: res.data,
+    });
+    return res;
+  } catch (error) {
+    console.error(error);
+  }
 };
 
-// export const logout = async (dataUser) => {
-//     try {
-//         let res = await axios.post(API_URL + "/users/login", dataUser);
-//         return res;
-//     } catch (error) {
-//         console.log(error);
-//     }
-// };
+export const updateUser = async (_id, dataUser) => {
+  try {
+    const credentials = JSON.parse(
+      localStorage.getItem("redux_localstorage_simple_credentials")
+    );
+    let config = {
+      headers: { Authorization: credentials.token },
+    };
+    let res = await axios.put(` ${API_URL}/users/${_id}`, dataUser, config);
+    await store.dispatch({ type: MODIFY_CREDENTIALS, payload: res.data });
+
+    return res;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getUserInfo = async () => {
+  try {
+    const credentials = JSON.parse(
+      localStorage.getItem("redux_localstorage_simple_credentials")
+    );
+    let config = {
+      headers: { Authorization: credentials.token },
+    };
+    let res = await axios.get(`${API_URL}/users/_id`, config);
+    await store.dispatch({ type: USER_INFO, payload: res.data });
+
+    return res;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getUsers = async () => {
+  try {
+    const credentials = JSON.parse(
+      localStorage.getItem("redux_localstorage_simple_credentials")
+    );
+    let config = {
+      headers: { Authorization: credentials.token },
+    };
+    let res = await axios.get(`${API_URL}/users`, config);
+    store.dispatch({ type: GET_USERS, payload: res.data });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const deleteUser = async (_id) => {
+  try {
+    const credentials = JSON.parse(
+      localStorage.getItem("redux_localstorage_simple_credentials")
+    );
+    let config = {
+      headers: { Authorization: credentials.token },
+    };
+    const res = await axios.delete(API_URL + `/users/delete/${_id}`, config);
+    store.dispatch({ type: DELETE_USER, payload: res.data });
+    return res;
+  } catch (error) {
+    console.log(error);
+  }
+};
--- src/utility.js
+++ src/utility.js
@@ -1,1 +1,6 @@
 export const API_URL = "https://mernsocialbackend.herokuapp.com";
+
+// const API_URL =
+//   process.env.NODE_ENV === "production"
+//     ? "https://mernsocialbackend.herokuapp.com"
+//     : "http://localhost:5500";
