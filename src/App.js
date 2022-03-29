93c3b72a28ee5f78ab16048d8a4b6b5fd462e273
raquelMoya <rakelms90@gmail.com>
2022-03-29 18:01:17 +0200

    Starting with Header

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Register from './containers/Register/Register'
 import Login from './containers/Login/Login';
 import Home from './containers/Home/Home';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Header/>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/register" element={<Register />} />
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,20 @@
+import React, {useState} from 'react';
+
+
+import './Header.css';
+
+const Header = () => {
+
+  
+        return (
+            <div className='designHeader'>
+              Soy Header
+            </div>
+        )
+}
+
+    
+
+
+
+export default Header;
