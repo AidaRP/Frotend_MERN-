5db9dd2be84b3b3a02737b9078f1f631a83e078c
raquelMoya <rakelms90@gmail.com>
2022-03-29 18:28:50 +0200

    Header ready

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,20 +1,65 @@
 import React, {useState} from 'react';
+import { LOGOUT } from "../../redux/types";
+import {useNavigate} from 'react-router-dom';
+import {connect} from 'react-redux';
 
 
 import './Header.css';
 
-const Header = () => {
+const Header = (props) => {
 
-  
+    let navigate = useNavigate();
+
+    const surf = (lugar) => {
+
+        setTimeout(()=> {
+            navigate(lugar);
+        }, 200);
+    }
+
+    const logOut = () => { 
+         props.dispatch({type:LOGOUT});
+        
+         setTimeout(()=>{
+             navigate("/");
+         },1000);
+    }
+
+    if(!props.token){
         return (
             <div className='designHeader'>
-              Soy Header
+                <div className="headerSpace genreDesign">
+                
+                </div>
+                <div className="headerSpace"></div>
+                <div className="headerSpace linksDesign">
+                    <div className="link" onClick={()=>surf("/login")}>Login</div>
+                    <div className="link" onClick={()=>surf("/register")}>Register</div>   
+                </div>
             </div>
         )
+    }else {
+        return (
+            <div className='designHeader'>
+                  <div className="headerSpace genreDesign">
+               
+                </div>
+                <div className="headerSpace"></div>
+                <div className="headerSpace linksDesign">
+                    <div className="link" onClick={()=>surf("/profile")}>{props.user.nickname}</div>
+                    <div className="link" onClick={()=>logOut()}>Logout</div>  
+                    <div className="link"onClick={()=>surf("/")}>Home</div>   
+                </div>
+            </div>
+        )
+    }
 }
+
+
 
     
 
 
 
-export default Header;
+const mapStateToProps = (state) => ({ user: state.credentials.user, token: state.credentials.token });
+export default connect(mapStateToProps)(Header);
