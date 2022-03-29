a3f9f89894c63055c12ffb3c909a1c9bc8bc3cb9
AidaRP <90700748+AidaRP@users.noreply.github.com>
2022-03-29 18:04:05 +0200

    need change to develop for pull

--- src/App.js
+++ src/App.js
@@ -3,16 +3,20 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Register from './containers/Register/Register'
 import Login from './containers/Login/Login';
 import Home from './containers/Home/Home';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
         </Routes>
+        <Footer/>
       </BrowserRouter>
     </div>
   );
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {useNavigate} from 'react-router-dom';
+import axios from 'axios';
+import './Header.css';
+
+const Header = () => {
+        return (
+            <div className='designHeader'>
+             Soy Header hijos de puta
+            </div>
+        )
+   
+    }
+
+export default Header;
