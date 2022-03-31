ecdc5467a9ad8672c2888e5574c59aeb0c24cc2f
XNycX <nycx2009@hotmail.com>
2022-03-31 23:18:57 +0200

    feat: Profile.jsx finished, update profile working

--- src/containers/Profile/Profile.jsx
+++ src/containers/Profile/Profile.jsx
@@ -2,125 +2,127 @@ import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { connect } from "react-redux";
 import { updateUser, getUserInfo } from "../../redux/actions/user";
-import { Modal, Button, Input } from "antd";
+import { Modal, Button, Input, notification } from "antd";
 import "./Profile.css";
 
-
 const Profile = (props) => {
-    let navigate = useNavigate();
-    //Hooks
-    const [dataUser, setDataUser] = useState({
-        nickname: props.user.nickname,
-        image_path: props.user.image_path,
-        city: props.user.city,
-        email: props.user.email,
-        followers: props.user.followers,
-        following: props.user.following,
-    });
-    
-    
+  let navigate = useNavigate();
+  //Hooks
+  const [dataUser, setDataUser] = useState({
+    nickname: props.user.nickname,
+    image_path: props.user.image_path,
+    city: props.user.city,
+    email: props.user.email,
+    followers: props.user.followers,
+    following: props.user.following,
+  });
 
-    const fillData = (e) => {
-        setDataUser({ ...dataUser, [e.target.name]: e.target.value });
-    };
+  const [visible, setVisible] = useState(false);
 
-    // useEffect(() => {
-    //     getUserInfo();
-    // }, []);
+  const fillData = (e) => {
+    setDataUser({ ...dataUser, [e.target.name]: e.target.value });
+  };
 
-    useEffect(() => {
-        if (props.user.token === "") {
-            navigate("/");
-        }
-    });
-    const onSubmit = async () => {
-        try {
-            const res = await updateUser(props.user._id, dataUser);
-            if (res) {
-                getUserInfo();
-            };
-            
-        } catch (error) {
-            console.log(error);
-        }
-    };
-    const [visible, setVisible] = useState(false);
+  useEffect(() => {
+    getUserInfo();
+  }, []);
 
-    return (
-        <div className="container">
-            <div className="card">
-                <h1>{props.user.image_path}</h1>
-                <p>
-                    <b>Nickname: </b>
-                    {dataUser.nickname}
-                </p>
-                <p>
-                    <b>City: </b>
-                    {dataUser.city}
-                </p>
-                <p>
-                    <b>Email: </b>
-                    {dataUser.email}
-                </p>
-                <p>
-                    <b>Followers: </b>
-                    {dataUser.followers}
-                </p>
-                <p>
-                    <b>Following: </b>
-                    {dataUser.following}
-                </p>
-                <Button type="primary" onClick={() => setVisible(true)}>
-                    Edit Profile
-                </Button>
-                <Modal
-                    title="Edit Profile"
-                    visible={visible}
-                    onOk={() => onSubmit()}
-                    onCancel={() => setVisible(false)}
-                >
-                    <p><b>Nickname:</b></p>
-                    <Input
-                        name="nickname"
-                        type="text"
-                        variant="filled"
-                        autoComplete="off"
-                        value={dataUser.nickname || ""}
-                        onChange={(e) => {
-                            fillData(e);
-                          }}
-                    />
-                    <p><b>City:</b></p>
-                    <Input
-                        name="city"
-                        type="text"
-                        variant="filled"
-                        autoComplete="off"
-                        value={dataUser.city || ""}
-                        onChange={(e) => {
-                            fillData(e);
-                          }}
-                    />
-                    <p><b>Image:</b></p>
-                    <Input
-                        name="image_path"
-                        type="url"
-                        variant="filled"
-                        autoComplete="off"
-                        value={dataUser.image_path || ""}
-                        onChange={(e) => {
-                            fillData(e);
-                          }}
-                    />
-                    <Button type="primary" onClick={() => onSubmit()}>
-                    Update
-                </Button>
-                </Modal>
-            </div>
-        </div>
-    );
-}
-                
+  useEffect(() => {
+    if (props.user.token === "") {
+      navigate("/");
+    }
+  });
+  const onSubmit = async () => {
+    try {
+      const res = await updateUser(props.user._id, dataUser);
+      if (res) {
+        getUserInfo();
+          setVisible(false);
+          notification.success({ message: "Perfil actualizado con éxito" });
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div className="container">
+      <div className="card">
+        <h1>{props.user.image_path}</h1>
+        <p>
+          <b>Nickname: </b>
+          {dataUser.nickname}
+        </p>
+        <p>
+          <b>City: </b>
+          {dataUser.city}
+        </p>
+        <p>
+          <b>Email: </b>
+          {dataUser.email}
+        </p>
+        <p>
+          <b>Followers: </b>
+          {dataUser.followers}
+        </p>
+        <p>
+          <b>Following: </b>
+          {dataUser.following}
+        </p>
+        <Button type="primary" onClick={() => setVisible(true)}>
+          Edit Profile
+        </Button>
+        <Modal
+          title="Edit Profile"
+          visible={visible}
+          onOk={() => onSubmit()}
+          onCancel={() => setVisible(false)}
+        >
+          <p>
+            <b>Nickname:</b>
+          </p>
+          <Input
+            name="nickname"
+            type="text"
+            variant="filled"
+            autoComplete="off"
+            value={dataUser.nickname || ""}
+            onChange={(e) => {
+              fillData(e);
+            }}
+          />
+          <p>
+            <b>City:</b>
+          </p>
+          <Input
+            name="city"
+            type="text"
+            variant="filled"
+            autoComplete="off"
+            value={dataUser.city || ""}
+            onChange={(e) => {
+              fillData(e);
+            }}
+          />
+          <p>
+            <b>Image:</b>
+          </p>
+          <Input
+            name="image_path"
+            type="url"
+            variant="filled"
+            autoComplete="off"
+            value={dataUser.image_path || ""}
+            onChange={(e) => {
+              fillData(e);
+            }}
+          />
+        </Modal>
+      </div>
+    </div>
+  );
+};
+
 const mapStateToProps = (state) => ({
   user: state.credentials.user,
   token: state.credentials.token,
